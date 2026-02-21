9e85a12dbb6a3995e5e70d176c5ea7afea4debcb
Kamal Sharma <kamal.sh1432@gmail.com>
2026-02-21 21:17:31 +0530

    Updated CORS settings

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  # local dev
+    "https://decisio-frontend.vercel.app",  # we will update later
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
